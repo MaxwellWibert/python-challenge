09f11b08f7d00c447c197e0ab1de5003ce17d1f1
Maxwell T Wibert <maxwelltwibert@Maxwells-MacBook-Pro.local>
2019-12-05 23:42:35 -0600

    cleaned up most of the file structures, running into some minor bug with the main.py file in **PyParagraph**, strange since it runs just fine in the corresponding ipynb file. Will investigate tomorrow.

--- **PyBank**/main.py
+++ **PyBank**/main.py
@@ -1,14 +1,115 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[46]:
+
+
+#importing dependencies
 import os
 import csv
 
-dataPath = os.path.join('.','Resources','budget_data.csv')
 
-with open(dataPath, 'r') as csvfile:
+# In[3]:
+
+
+#reference relative file path
+file = "./Resources/budget_data.csv"
+
+
+# In[34]:
+
+
+table = []
+with open(file, 'r') as csvfile:
     text = csvfile.read()
-    print(text)
 
     rows = text.split("\n")
-    print (rows)
 
-    table = rows.map(lambda row: row.split(","))
-    print(table)
+    for row in rows:
+        table.append(row.split(","))
+
+
+# In[34]:
+
+
+csvfile.close
+
+
+# In[35]:
+
+
+header = table.pop(0)
+
+
+# In[36]:
+
+
+# In[42]:
+
+
+#Removing invalid rows, changing datatype of profit column from string to integer
+index = len(table) - 1
+while index >= 0:
+    if(len(table[index]) != 2):
+        table.pop(index)
+    else:
+        table[index][1] = int(table[index][1])
+    index-=1
+    
+
+
+# In[43]:
+
+
+
+# In[64]:
+
+
+total_profit = 0
+running_max = table[1][1]-table[0][1]
+running_min = table[1][1]-table[0][1]
+running_arg_max = 1
+running_arg_min = 1
+month_count = len(table)
+#looping over table to find max, min change, cumulative profit
+for index, row in enumerate(table): 
+    row_profit = row[1]
+    total_profit += row_profit
+    if index > 0:
+        previous_profit = table[index-1][1]
+        delta = row_profit-previous_profit
+        if running_max < delta:
+            running_max = delta
+            running_arg_max = index
+        if running_min > delta:
+            running_min = delta
+            running_arg_min = index
+            
+first_row = table[0]
+final_row = table[month_count-1]
+
+average_profit_change = format((final_row[1]-first_row[1])/month_count, ".2f")
+
+
+# In[72]:
+
+
+#generating report based on calculated data
+report_string = f"\nFinancial Analysis \n{'-'*20}\n"f"Total Months: {month_count}\n"f"Total Profit: {total_profit} \n"f"Average Change: {average_profit_change}\n"f"Greatest Increase in Profits: {table[running_arg_max][0]} (${running_max})\n"f"Greatest Decrease in Profits: {table[running_arg_min][0]} (${running_min})"
+
+
+# In[73]:
+
+
+#printing and writing report string to file
+print(report_string)
+with open("account_report.txt", "w") as report:
+    report.write(report_string)
+report.close()
+
+
+# In[ ]:
+
+
+
+
---
+++ **PyBoss**/main.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+import os
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
+
+
+# In[2]:
+
+
+file = "./Resources/employee_data.csv"
+
+
+# In[4]:
+
+
+table = []
+with open(file, "r") as io:
+    file_text = io.read()
+    rows = file_text.split("\n")
+    for row in rows:
+        data = row.split(",")
+        if len(data) == 5:
+            table.append(data)
+
+
+# In[6]:
+
+
+header = table.pop(0)
+
+
+# In[24]:
+
+
+new_table = []
+new_table.append(["ID", "First Name", "Last Name", "DOB", "SSN", "State"])
+for row in table:
+    new_row = []
+    
+    new_row.append(row[0])
+    
+    name = row[1].split(" ")
+    new_row += name
+    
+    DOB = row[2]
+    new_DOB = "/".join(DOB.split("-"))
+    new_row.append(new_DOB)
+    
+    SSN = row[3]
+    new_SSN = f"***-**-{SSN[-4:]}"
+    new_row.append(new_SSN)
+    
+    state = row[4]
+    new_state =us_state_abbrev[state]
+    new_row.append(new_state)
+    
+    new_table.append(new_row)
+
+
+# In[25]:
+
+
+
+# In[26]:
+
+
+new_rows = []
+for row in new_table:
+    new_rows.append(",".join(row))
+results_string = "\n".join(new_rows)
+with open("results.csv", "w") as results:
+    results.write(results_string)
+results.close()
+
+print("Program Complete")
+# In[ ]:
+
+
+
+
